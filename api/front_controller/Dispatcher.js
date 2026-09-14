419659e5c5f2d4e927d62019c59786fb6b7c5723
DmitryBelkevich <harleydav@mail.ru>
2026-09-14 20:58:52 +0300

    Update Dispatcher.js

--- api/front_controller/Dispatcher.js
+++ api/front_controller/Dispatcher.js
@@ -1,4 +1,12 @@
+import MainController from "../controllers/MainController.js";
+
 export default class Dispatcher {
+  #routes[];
+
+  constructor() {
+    this.#routes[""] = new MainController();
+  }
+  
   dispatch(request) {
     const appName = "site";
     const routeName = request.slice(("/" + appName).length, -1);
