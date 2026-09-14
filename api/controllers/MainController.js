c5702f1547de94d1b677071d434ca9fd1d926c2d
DmitryBelkevich <harleydav@mail.ru>
2026-09-14 22:01:44 +0300

    Update MainController.js

--- api/controllers/MainController.js
+++ api/controllers/MainController.js
@@ -3,6 +3,9 @@ import MainView from "../views/MainView.js";
 export default class MainController {
   async init() {
     const mainView = new MainView();
+
+    mainView.setHeader("Header");
+    mainView.setMain("Main");
     mainView.setFooter("Footer");
   }
 }
