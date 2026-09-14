05e195762d18bea359acab8b78ff0734f7dd07dc
DmitryBelkevich <harleydav@mail.ru>
2026-09-14 21:31:24 +0300

    Update MainView.js

--- api/views/MainView.js
+++ api/views/MainView.js
@@ -1,11 +1,13 @@
 export default class MainView {
   constructor() {
     this.header = document.createElement("header");
+    this.header.textContent = "Header";
     
     this.main = document.createElement("main");
     this.main.textContent = "Main";
 
     this.footer = document.createElement("footer");
+    this.footer.textContent = "Footer";
     
     document.body.append(
       this.header,
