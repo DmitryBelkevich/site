f98e620a0ac3cd3bd93125f55b8476ca5f54916a
DmitryBelkevich <harleydav@mail.ru>
2026-09-14 22:46:36 +0300

    Update MainController.js

--- api/controllers/MainController.js
+++ api/controllers/MainController.js
@@ -2,18 +2,7 @@ import MainView from "../views/MainView.js";
 import HtmlLoader from "../loaders/HtmlLoader.js";
 
 export default class MainController {
-  async init() {
+  init() {
     const mainView = new MainView();
-
-    const htmlLoader = new HtmlLoader();
-
-    const header = "Header";
-    mainView.setHeader(header);
-
-    const main = await htmlLoader.load("./api/views/templates/main.html");
-    mainView.setMain(main);
-
-    const footer = await htmlLoader.load("./api/views/templates/footer.html");
-    mainView.setFooter(footer);
   }
 }
