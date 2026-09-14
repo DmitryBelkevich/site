105e5b087f94e369ef7a133bcd203b55a059ee64
DmitryBelkevich <harleydav@mail.ru>
2026-09-14 20:15:32 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import FrontController from "./FrontController.js";
+import FrontController from "./api/front_controller/FrontController.js";
 
 function main() {
   const frontController = new FrontController();
