9d27a78cc5e2e6911f252eeb3d8c3a80ee126fab
DmitryBelkevich <harleydav@mail.ru>
2026-09-14 19:41:51 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import FrontController from "./FrontController.js";
 
 function main() {
   const frontController = new FrontController();
+  
+  frontController.handleRequest("/main");
 }
 
 const app = main();
