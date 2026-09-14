7e14007458d45867382c5b1924690eec06491dfe
DmitryBelkevich <harleydav@mail.ru>
2026-09-14 20:41:15 +0300

    Update Dispatcher.js

--- api/front_controller/Dispatcher.js
+++ api/front_controller/Dispatcher.js
@@ -1,7 +1,7 @@
 export default class Dispatcher {
   dispatch(request) {
     const appName = "site";
-    const routeName = path.slice(("/" + appName).length, -1);
+    const routeName = request.slice(("/" + appName).length, -1);
 
     console.log(routeName);
   }
