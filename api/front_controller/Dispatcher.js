22c77c89bf0f415a885701cc02b7d46b9974d09d
DmitryBelkevich <harleydav@mail.ru>
2026-09-15 04:36:06 +0300

    Update Dispatcher.js

--- api/front_controller/Dispatcher.js
+++ api/front_controller/Dispatcher.js
@@ -1,10 +1,12 @@
 import MainController from "../controllers/MainController.js";
+import ContactsController from "../controllers/ContactsController.js";
 
 export default class Dispatcher {
   #routes = [];
 
   constructor() {
     this.#routes[""] = new MainController();
+    this.#routes["/contacts"] = new MainController();
   }
   
   dispatch(request) {
