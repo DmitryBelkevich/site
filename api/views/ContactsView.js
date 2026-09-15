429984a10cd78dad3724f3f9b58519e1ae0fd301
DmitryBelkevich <harleydav@mail.ru>
2026-09-15 04:52:08 +0300

    Update ContactsView.js

--- api/views/ContactsView.js
+++ api/views/ContactsView.js
@@ -22,7 +22,7 @@ export default class ContactsView {
     cssLoader.load("./api/views/css/header.css");
     this.setHeader(header);
 
-    const main = await htmlLoader.load("./api/views/templates/main.html");
+    const main = await htmlLoader.load("./api/views/templates/contacts/main.html");
     cssLoader.load("./api/views/css/main.css");
     this.setMain(main);
 
