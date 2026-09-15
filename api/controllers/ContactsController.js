7469682376e892bf341a662c4a15fb5cd5719992
DmitryBelkevich <harleydav@mail.ru>
2026-09-15 04:38:41 +0300

    Update ContactsController.js

--- api/controllers/ContactsController.js
+++ api/controllers/ContactsController.js
@@ -1,8 +1,8 @@
 import ContactsView from "../views/ContactsView.js";
 
-export class ContactsController {
+export default class ContactsController {
   init() {
-    // const view = new ContactsView();
-    // view.init();
+    const view = new ContactsView();
+    view.init();
   }
 }
